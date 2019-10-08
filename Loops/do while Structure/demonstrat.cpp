69c2b7a4724ef9bc4873365fe6709d8c4586e00f
nikhil269 <nikhilchaudhary12155@gmail.com>
2019-10-08 17:55:23 +0530

    New

---
+++ Loops/do while Structure/demonstrat.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+   /* local variable Initialization */   int n = 1,times=5;
+
+   /* while loops execution */   while( n <= times )
+   {
+      cout << "C++ while loops: " << n <<endl;
+      n++;
+   }
+   return 0;
+}
